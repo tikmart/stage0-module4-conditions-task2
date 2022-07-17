7ed9fc1cb4d5f3af0f76562821f104548694bec5
tikmart <t.martirosyan012@gmail.com>
2022-07-17 22:51:55 +0400

    Task 2 fifth commit

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -3,22 +3,14 @@ package school.mjc.stage0.conditions.task2;
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
         from1UpTo1000 = 499;
-        if (from1UpTo1000 < 500){
-
-            System.out.println("Eagle");
-        }
-
-        from1UpTo1000 = 500;
-        if (from1UpTo1000 == 500){
-
-            System.out.println("Tail");
-        }
-
-        from1UpTo1000 = 501;
 
         if (from1UpTo1000 > 500) {
 
             System.out.println("Tail");
+        }
+
+        else {
+            System.out.println("Eagle");
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -4,19 +4,14 @@ public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
 
         first = 12;
+
         second = -400;
 
-
-        if (first > second){
+        if (first > second) {
             System.out.println(first);
         }
-
-        first = -400;
-        second = 1;
-        if (first < second) {
+        else {
             System.out.println(second);
         }
-
-
     }
 }
